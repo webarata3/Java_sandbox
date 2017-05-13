3dbaa71fee93085b1c2b5d506f84a3f27d3f637b
webarata3 <webarata3@gmail.com>
2017-05-13 14:52:59 +0900

    ファイル保存ダイアログ

--- OekakiFx/src/main/java/link/webarata3/fx/oekaki/OekakiController.java
+++ OekakiFx/src/main/java/link/webarata3/fx/oekaki/OekakiController.java
@@ -1,16 +1,25 @@
 package link.webarata3.fx.oekaki;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.imageio.ImageIO;
+
 import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class OekakiController implements Initializable {
     @FXML
@@ -30,7 +39,23 @@ public class OekakiController implements Initializable {
     }
 
     @FXML
-    public void onActionExit(ActionEvent event) {
+    private void onActionSaveAs(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().addAll(new ExtensionFilter("画像ファイル", "*.png", "*.jpg", "*.gif"));
+        File selectedFile = fileChooser.showSaveDialog(canvas.getScene().getWindow());
+        if (selectedFile != null) {
+            WritableImage img = canvas.snapshot(new SnapshotParameters(), null);
+            try {
+                ImageIO.write(SwingFXUtils.fromFXImage(img, null), "png", selectedFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    private void onActionExit(ActionEvent event) {
         Platform.exit();
     }
 
