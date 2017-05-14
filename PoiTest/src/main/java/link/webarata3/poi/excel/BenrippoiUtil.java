594443d493ce5f20560077865ed46c1091b6cd0f
webarata3 <webarata3@gmail.com>
2017-05-14 17:36:07 +0900

    cellToStringの追加

--- PoiTest/src/main/java/link/webarata3/poi/excel/BenrippoiUtil.java
+++ PoiTest/src/main/java/link/webarata3/poi/excel/BenrippoiUtil.java
@@ -6,16 +6,27 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.CellValue;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
+/**
+ * Apache POIのラッパークラスです。
+ * 
+ * @author webarata3
+ */
 public class BenrippoiUtil {
     public static Optional<Workbook> open(String fileName) {
         try {
@@ -35,7 +46,7 @@ public class BenrippoiUtil {
         }
     }
 
-    public static String cellIndexToCellName(int x, int y) {
+    public static String cellIndexToCellLabel(int x, int y) {
         String cellName = dec26(x, 0);
         return cellName + (y + 1);
     }
@@ -44,7 +55,21 @@ public class BenrippoiUtil {
         return (num > 25 ? dec26(num / 26, 1) : "") + String.valueOf((char) ('A' + (num - first) % 26));
     }
 
-    public Row getRow(Sheet sheet, int n) {
+    public static Cell getCell(Sheet sheet, String cellLabel) {
+        Pattern p1 = Pattern.compile("([a-zA-Z]+)([0-9]+)");
+        Matcher matcher = p1.matcher(cellLabel);
+        matcher.find();
+
+        String reverseString = new StringBuilder(matcher.group(1).toUpperCase()).reverse().toString();
+        int x = IntStream.range(0, reverseString.length()).map((i) -> {
+            int delta = reverseString.charAt(i) - 'A' + 1;
+            return delta * (int) Math.pow(26.0, (double) i);
+        }).reduce(-1, (v1, v2) -> v1 + v2);
+
+        return getCell(sheet, x, Integer.parseInt(matcher.group(2)) - 1);
+    }
+
+    public static Row getRow(Sheet sheet, int n) {
         Row row = sheet.getRow(n);
         if (row != null) {
             return row;
@@ -59,5 +84,50 @@ public class BenrippoiUtil {
             return cell;
         }
         return row.createCell(x, CellType.BLANK);
+    }
+
+    public static String normalizeNumericString(double numeric) {
+        // 44.0のような数値を44として取得するために、入力された数値と小数点以下を切り捨てた数値が
+        // 一致した場合には、intにキャストして、小数点以下が表示されないようにしている
+        if (numeric == Math.ceil(numeric)) {
+            return String.valueOf((int) numeric);
+        }
+        return String.valueOf(numeric);
+    }
+
+    public static CellValue getFomulaCellValue(Cell cell) {
+        Workbook wb = cell.getSheet().getWorkbook();
+        CreationHelper helper = wb.getCreationHelper();
+        FormulaEvaluator evaluator = helper.createFormulaEvaluator();
+        return evaluator.evaluate(cell);
+    }
+
+    public static String cellToString(Cell cell) {
+        switch (cell.getCellTypeEnum()) {
+        case STRING:
+            return cell.getStringCellValue();
+        case NUMERIC:
+            return normalizeNumericString(cell.getNumericCellValue());
+        case BOOLEAN:
+            return String.valueOf(cell.getBooleanCellValue());
+        case BLANK:
+            return "";
+        case FORMULA:
+            CellValue cellValue = getFomulaCellValue(cell);
+            switch (cellValue.getCellTypeEnum()) {
+            case STRING:
+                return cellValue.getStringValue();
+            case NUMERIC:
+                return normalizeNumericString(cellValue.getNumberValue());
+            case BOOLEAN:
+                return String.valueOf(cellValue.getBooleanValue());
+            case BLANK:
+                return "";
+            default: // _NONE, ERROR
+                throw new IllegalStateException("cellはStringに変換できません");
+            }
+        default: // _NONE, ERROR
+            throw new IllegalStateException("cellはStringに変換できません");
+        }
     }
 }
--- PoiTest/src/test/java/link/webarata3/poi/excel/BenrippoiUtilTest.java
+++ PoiTest/src/test/java/link/webarata3/poi/excel/BenrippoiUtilTest.java
@@ -4,10 +4,11 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Optional;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,11 +18,19 @@ public class BenrippoiUtilTest {
     @Rule
     public TemporaryFolder tempFolder = new TemporaryFolder();
 
-    @Test
-    public void openTest() throws IOException {
+    private Optional<Workbook> getOptionalWorkbook(String fileName) throws Exception {
         File tempFile = new File(tempFolder.getRoot(), "temp.xlsx");
-        Files.copy(this.getClass().getResourceAsStream("book1.xlsx"), tempFile.toPath());
-        Optional<Workbook> optionalWb = BenrippoiUtil.open(Files.newInputStream(tempFile.toPath()));
+        Files.copy(this.getClass().getResourceAsStream(fileName), tempFile.toPath());
+        return BenrippoiUtil.open(Files.newInputStream(tempFile.toPath()));
+    }
+
+    private Workbook getWorkbook(String fileName) throws Exception {
+        return getOptionalWorkbook(fileName).get();
+    }
+
+    @Test
+    public void openTest() throws Exception {
+        Optional<Workbook> optionalWb = getOptionalWorkbook("book1.xlsx");
         assertThat(optionalWb.isPresent(), is(true));
         Workbook wb = optionalWb.get();
         wb.close();
@@ -29,11 +38,20 @@ public class BenrippoiUtilTest {
 
     @Test
     public void cellIndexToCellNameTest() {
-        assertThat(BenrippoiUtil.cellIndexToCellName(0, 0), is("A1"));
-        assertThat(BenrippoiUtil.cellIndexToCellName(1, 0), is("B1"));
-        assertThat(BenrippoiUtil.cellIndexToCellName(2, 0), is("C1"));
-        assertThat(BenrippoiUtil.cellIndexToCellName(26, 0), is("AA1"));
-        assertThat(BenrippoiUtil.cellIndexToCellName(27, 0), is("AB1"));
-        assertThat(BenrippoiUtil.cellIndexToCellName(28, 0), is("AC1"));
+        assertThat(BenrippoiUtil.cellIndexToCellLabel(0, 0), is("A1"));
+        assertThat(BenrippoiUtil.cellIndexToCellLabel(1, 0), is("B1"));
+        assertThat(BenrippoiUtil.cellIndexToCellLabel(2, 0), is("C1"));
+        assertThat(BenrippoiUtil.cellIndexToCellLabel(26, 0), is("AA1"));
+        assertThat(BenrippoiUtil.cellIndexToCellLabel(27, 0), is("AB1"));
+        assertThat(BenrippoiUtil.cellIndexToCellLabel(28, 0), is("AC1"));
+    }
+
+    @Test
+    public void test() throws Exception {
+        Workbook wb = getWorkbook("book1.xlsx");
+        Sheet sheet = wb.getSheet("Sheet1");
+        Cell cell = BenrippoiUtil.getCell(sheet, "B1");
+        System.out.println(BenrippoiUtil.cellToString(cell));
+        System.out.println(cell.getStringCellValue());
     }
 }
