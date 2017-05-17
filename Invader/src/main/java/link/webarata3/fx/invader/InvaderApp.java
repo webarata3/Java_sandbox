47ec3e5feed2d98056339fd939af82e151215ebb
webarata3 <webarata3@gmail.com>
2017-05-17 18:35:39 +0900

    Invaderの追加

---
+++ Invader/src/main/java/link/webarata3/fx/invader/InvaderApp.java
@@ -0,0 +1,27 @@
+package link.webarata3.fx.invader;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class InvaderApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Invader.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+
+        InvaderController invaderController = (InvaderController) loader.getController();
+        invaderController.setScene(scene);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ Invader/src/main/java/link/webarata3/fx/invader/InvaderController.java
@@ -0,0 +1,51 @@
+package link.webarata3.fx.invader;
+
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InvaderController implements Initializable {
+    @FXML
+    private Canvas canvas;
+
+    private GraphicsContext gc;
+
+    private Scene scene;
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+        scene.setOnKeyPressed(event -> {
+            System.out.println(event.getCode());
+        });
+    }
+
+    @FXML
+    private void onKeyPressed(KeyEvent keyEvent) {
+        System.out.println(keyEvent.getCode());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        gc = canvas.getGraphicsContext2D();
+        gc.setLineWidth(3);
+    }
+}
