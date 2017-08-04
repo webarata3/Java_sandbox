c2948f2fc696ac58ca7549234dcbd65095560636
webarata3 <webarata3@gmail.com>
2017-08-04 18:43:09 +0900

    propertyを使わないように変更

--- JcifsTest/src/main/java/link/webarata3/jcifs/JcifsTest.java
+++ JcifsTest/src/main/java/link/webarata3/jcifs/JcifsTest.java
@@ -3,26 +3,34 @@ package link.webarata3.jcifs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
 
 import jcifs.Config;
+import jcifs.smb.NtlmPasswordAuthentication;
+import jcifs.smb.SmbFile;
 import jcifs.smb.SmbFileInputStream;
 
 public class JcifsTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         Properties properties = new Properties();
-        URL url = ClassLoader.getSystemClassLoader().getResource("jcifs.properties");
+        URL propertyUrl = ClassLoader.getSystemClassLoader().getResource("jcifs.properties");
         try {
-            properties.load(url.openStream());
+            properties.load(propertyUrl.openStream());
         } catch (IOException e) {
             e.printStackTrace();
             return;
         }
 
-        Config.setProperties(properties);
+//        Config.setProperties(properties);
 
-        try (SmbFileInputStream sfis = new SmbFileInputStream("smb://192.168.1.11/public/鍵.txt");
+        String url = "smb://" + properties.getProperty("server") + "/public/old-ゴミ/鍵.txt";
+        NtlmPasswordAuthentication auth =
+            new NtlmPasswordAuthentication("",properties.getProperty("username"), properties.getProperty("password"));
+
+        SmbFile smbFile = new SmbFile(url, auth);
+        try (SmbFileInputStream sfis = new SmbFileInputStream(smbFile);
                 BufferedReader br = new BufferedReader(new InputStreamReader(sfis))) {
 
             String line = null;
